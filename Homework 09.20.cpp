689743405b69e2ef0f9d203608aa973689826be0
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2024-09-23 19:56:23 +0300

    Create Homework 09.20.cpp

---
+++ Homework 09.20.cpp
@@ -0,0 +1,14 @@
+/*
+	// 1
+	int a, b = 0;
+	cout << "chislo: ";
+	cin >> a;
+	for (int i = a; i < 500; i++) {
+		b += i;
+	}
+	cout << b;
+	*/
+	// 2
+	int x, y;
+	cout << "chislo(x, y): ";
+	cin >> x >> y;
