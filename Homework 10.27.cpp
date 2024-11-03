846213d0d5d3ecdd858011bc5af9f0ebe8b980b5
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2024-11-03 15:38:44 +0200

    Update Homework 10.27.cpp

--- Homework 10.27.cpp
+++ Homework 10.27.cpp
@@ -1,77 +1,87 @@
-#include <math.h>
 #include <iostream>
 #include <time.h>
 #include <windows.h>
-#include <conio.h>
 using namespace std;
 
-struct date {
-	int y, m, d;
-};
-
-bool is_date_correct(date a) {
-	if (a.m > 12 || a.m < 1 || a.d < 1 || a.y < 0) {
-		return false;
-	}
-	else if (a.d > 28) {
-		if (a.d > 31 && a.m == 1)
-			return false;
-		if (a.d > 28 && a.m == 2) {
-			if (a.d > 29)
-				return false;
-			if (a.y % 400 == 0 || a.y % 100 != 0 && a.y % 4 == 0)
-				return true;
-		}
-		if (a.d > 31 && a.m == 3)
-			return false;
-		if (a.d > 30 && a.m == 4)
-			return false;
-		if (a.d > 31 && a.m == 5)
-			return false;
-		if (a.d > 30 && a.m == 6)
-			return false;
-		if (a.d > 31 && a.m == 7)
-			return false;
-		if (a.d > 31 && a.m == 8)
-			return false;
-		if (a.d > 30 && a.m == 9)
-			return false;
-		if (a.d > 31 && a.m == 10)
-			return false;
-		if (a.d > 30 && a.m == 11)
-			return false;
-		if (a.d > 31 && a.m == 12)
-			return false;
-	}
-	return true;
+void SetColor(int textColor, int bgColor)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole,
+		(bgColor << 4) | textColor);
 }
 
+struct date {
+	int d, m, y;
+
+	bool isCorrect(){
+		if (m < 1 || m > 12)
+			return false;
+		if (d < 1 || d > getDaysInMonth())
+			return false;
+		if (y < 0)
+			return false;
+
+		return true;
+	}
+
+	int days() {
+		int years_in_days = y * 365;
+		for (int i = 0; i <= y; i++)
+			if (i % 400 == 0 || i % 100 != 0 && i % 4 == 0)
+				years_in_days++;
+
+		int mounth_in_days = 0;
+		for (int i = 0; i < m; i++) {
+			mounth_in_days += getDaysInMonth(i, y);
+		}
+		return years_in_days + mounth_in_days + d;
+	}
+
+	bool isLeapYear(){
+		return isLeapYear(y);
+	}
+
+	bool isLeapYear(int _year){
+		return _year % 400 == 0 || _year % 4 == 0 && _year % 100 != 0;
+	}
+
+	int getDaysInMonth(){
+		return getDaysInMonth(m, y);
+	}
+
+	int getDaysInMonth(int _month, int year){
+		switch (_month)
+		{
+		case 1: return 31;
+		case 2: return isLeapYear(year) ? 29 : 28;
+		case 3: return 31;
+		case 4: return 30;
+		case 5: return 31;
+		case 6: return 30;
+		case 7: return 31;
+		case 8: return 31;
+		case 9: return 30;
+		case 10: return 31;
+		case 11: return 30;
+		case 12: return 31;
+		default: return 0;
+		}
+	}
+
+	void print_date(){
+		cout << (d < 10 ? "0" : "") << d << "." << (m < 10 ? "0" : "") << m << "." << y;
+	}
+};
+
 int riznica(date a, date b) {
-	int d_days = ((((b.y * 12) + b.m) * 30) + b.d) - ((((a.y * 12) + a.m) * 30) + a.d);
-	return d_days;
+	return a.days() - b.days();
 }
 
 int main()
 {
-	date data = { 2000, 2, 30 }, a, b;
-	cout << "First year mounth day";
-	cin >> a.y >> a.m >> a.d;
-	if (is_date_correct(a) == false)
-		cout << "Not Correct";
-	cout << "Seccond year mounth day";
-	cin >> b.y >> b.m >> b.d;
-	if (is_date_correct(b) == false)
-		cout << "Not Correct";
-	cout << riznica(a, b) << "days" << endl;
-
-
-	cout << "is date correct?" << endl;
-	cout << data.y << "." << data.m << "." << data.d << endl;
-	if (is_date_correct(data))
-		cout << "YES!!!";
-	else
-		cout << "No";
-
-
+	srand(time(0));
+	date a = { 2000, 2, 30 }, b = { 2010, 5, 10 };
+	cout << riznica(a, b);
+	
 	return 0;
 }
